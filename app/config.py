e012b62a6f1c44a1a9e32853ef3bab2a89bde50a
ivy_ck <ivanna.so.cervantes@gmail.com>
2023-11-12 00:06:19 -0500

    Requerimientos modificados

---
+++ app/config.py
@@ -0,0 +1,15 @@
+# config.py
+
+class Config:
+    SECRET_KEY = 'clave_secreta_para_proteger_sesiones'
+    DEBUG = True
+
+    # Configuración de la base de datos MS Access
+    DATABASE_CONNECTION_STRING = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=ESCRIBE AQUI TU RUTA DE LA BASE DE DATOS;'
+
+    ### EJEMPLO DE LA RUTA: DBQ=C:\USER\EXAMEN-iii-main\EXAMEN-iii-main\DB.accdb;'
+
+
+    @staticmethod
+    def init_app(app):
+        pass
